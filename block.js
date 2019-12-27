ac2953b2d5dfb529f5f09632a405a12f5b38d36a
richiesomething <51523117+richiesomething@users.noreply.github.com>
2019-12-26 18:13:10 -0800

    first commit
    excuses node modules

---
+++ block.js
@@ -0,0 +1,31 @@
+const { GENESIS_DATA } = require('./config');
+const cryptoHash = require('./crypto-hash')
+
+class Block {
+	constructor({timestamp, lastHash, hash, data}) {
+		this.timestamp = timestamp;
+		this.lastHash = lastHash;
+		this.hash = hash;
+		this.data = data;
+	}
+
+	static genesis() {
+		return new this(GENESIS_DATA);
+	}
+
+	static minedBlock({ lastBlock, data }) {
+		const timestamp = Date.now();
+		const lastHash = lastBlock.hash;
+
+		return new this({
+			timestamp,
+			lastHash,
+			data,
+			hash: cryptoHash(timestamp, lastHash, data)
+		});
+	}
+}
+
+
+
+module.exports = Block // node.js way of sharing files
